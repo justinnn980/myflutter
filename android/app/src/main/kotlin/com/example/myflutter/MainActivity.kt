096f4b2db6d31a68edca9ee965fdeb0b71968cbf
justinnn980 <justinnn980@gmail.com>
2025-10-10 17:04:46 +0900

    Update : 1.0.2+7

--- android/app/src/main/kotlin/com/example/myflutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/myflutter/MainActivity.kt
@@ -1,6 +1,5 @@
-package com.example.myflutter
+package com.booox.myflutter
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
